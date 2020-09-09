3e96e908bf91948351345dd4bfcc1511a5d71fb9
Kairat Satbekov <kairat.satbekov@Kairats-MacBook-Pro.local>
2020-09-09 06:20:40 +0600

    added solution to human-readable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
+const firstNineteen = [
+    '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+    'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen',
+    'eighteen', 'nineteen'
+];
+
+const tenDivs = [
+    '', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 
+    'seventy', 'eighty', 'ninety'
+];
+
+const thousands = ['', 'thousand', 'million', 'billion'];
+
 module.exports = function toReadable (number) {
-  
+  if (number === 0 || !number) return 'zero';
+
+  let message = '', count = 0;
+
+  while (number > 0) {
+    if (number % 1000 !== 0) {
+      message = `${extractNum(number % 1000)} ${thousands[count]} ${message}`;
+    }
+
+    number = Math.floor(number / 1000);
+    count++;
+  }
+
+  return message.trim();
+}
+
+const extractNum = number => {
+  if (number === 0) {
+    return '';
+  } 
+  else if (number < 20) {
+    return firstNineteen[number];
+  } 
+  else if (number < 100) {
+    return `${tenDivs[Math.floor(number/10)]} ${extractNum(number % 10)}`;
+  } 
+  else {
+    return `${firstNineteen[Math.floor(number/100)]} hundred ${extractNum(number%100)}`;
+  }
 }
